f3bfb9a43a145dbd8266b42dc33e3643401464ee
Sean Abong <sa2278@york.ac.uk>
2023-01-13 23:14:05 +0000

    cleaned up spreadsheet, modeled pricing and threads

--- amd cpu analysis/amd_cpu_analysis.py
+++ amd cpu analysis/amd_cpu_analysis.py
@@ -15,3 +15,19 @@ data = pd.read_csv("AMD CPU spreadsheet(cleaned).csv", encoding = 'latin1')
 ## this drops null values from the dataset   
 print(data.info())   
 
+coreNum = data["# of CPU Cores"].sum()
+threadNum = data["# of Threads"].sum() 
+baseClock = data["Base Clock(GHz)"].sum() 
+ 
+
+plt.figure(figsize=(10, 8))
+plt.style.use('fivethirtyeight')
+plt.title("Distribution of prices")
+sns.distplot(data['1kU Pricing(USD)'])
+plt.show()   
+
+plt.figure(figsize=(10, 8))
+plt.style.use('fivethirtyeight')
+plt.title("Distribution of threads")
+sns.distplot(data['# of Threads'])
+plt.show()  
