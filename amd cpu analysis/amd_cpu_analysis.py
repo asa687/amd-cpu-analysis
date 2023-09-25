1fac957d0d6ef7c98c861ea1e053d3bac597bd95
asa687 <85872834+asa687@users.noreply.github.com>
2023-09-25 14:24:34 +0100

    increasen the sample test size

--- amd cpu analysis/amd_cpu_analysis.py
+++ amd cpu analysis/amd_cpu_analysis.py
@@ -11,7 +11,7 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import PassiveAggressiveRegressor
 
-data = pd.read_csv("AMD CPU spreadsheet(cleaned with no nulls).csv", encoding = 'latin1') 
+data = pd.read_csv("AMD CPU spreadsheet(cleaned nulls are -1).csv", encoding = 'latin1') 
 ## this drops null values from the dataset  
 print(data.info())   
 
@@ -24,7 +24,7 @@ plt.figure(figsize=(10, 8))
 plt.style.use('fivethirtyeight')
 plt.title("Distribution of prices") 
 sns.distplot(data['1kU Pricing(USD)']) 
-## plt.xticks(np.arange(min(data['1kU Pricing(USD)']), max(data['1kU Pricing(USD)'])+1, 1250))
+plt.xticks(np.arange(0, max(data['1kU Pricing(USD)'])+1, 1250))
 plt.show()   
 
 plt.figure(figsize=(10, 8)) 
@@ -44,10 +44,11 @@ plt.show()
 ## model for a hypothetical cpu 
 x = np.array(data[['# of CPU Cores', '# of Threads', 'Base Clock(GHz)', 'L1 Cache (KB)', 'L2 Cache(MB)', 'L3 Cache(MB)']])
 y = np.array(data["1kU Pricing(USD)"])  
-xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.4, random_state=42)
 
 model = PassiveAggressiveRegressor()
 model.fit(xtrain, ytrain)
 model.score(xtest, ytest)  
 features = np.array([[8, 16, 4, 120, 11.5, 24.5]])
-prediction = model.predict(features)
+prediction = model.predict(features) 
+print(prediction)
