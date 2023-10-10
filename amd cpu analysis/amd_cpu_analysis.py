49e197cdac59c8896866a6f47c4d7ae8562cc56c
asa687 <85872834+asa687@users.noreply.github.com>
2023-10-10 16:59:56 +0100

    Created a second spreadsheet that replaces 0 for all values that are null in threads and base clock (they would be 0 in the cpu as the features do not exist). edited the lbfgs ml algorithm to have greater iteration numbers and 3 layers of reduced hidden node size.

--- amd cpu analysis/amd_cpu_analysis.py
+++ amd cpu analysis/amd_cpu_analysis.py
@@ -12,7 +12,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import PassiveAggressiveRegressor 
 from sklearn.neural_network import MLPClassifier
 
-data = pd.read_csv("AMD CPU spreadsheet(cleaned with no nulls).csv", encoding = 'latin1')  
+data = pd.read_csv("AMD CPU spreadsheet(cleaned with no nulls).csv", encoding = 'latin1')   
+data2 = pd.read_csv("AMD CPU spreadsheet(cleaned 2).csv", encoding = 'latin1')  
+cleanedData2 = data2.dropna(subset=['1kU Pricing(USD)'], inplace=False)
 ## this drops null values from the dataset  
 print(data.info())   
 
@@ -25,7 +27,7 @@ plt.figure(figsize=(10, 8))
 plt.style.use('fivethirtyeight')
 plt.title("Distribution of prices") 
 sns.distplot(data['1kU Pricing(USD)']) 
-plt.xticks(np.arange(0, max(data['1kU Pricing(USD)'])+1, 1250))
+plt.xticks(np.arange(0, max(data['1kU Pricing(USD)'])+1, 625))
 plt.show()   
 
 plt.figure(figsize=(10, 8)) 
@@ -42,27 +44,23 @@ sns.distplot(data['# of CPU Cores'])
 plt.xticks(np.arange(0, max(data['# of CPU Cores'])+1, 4))
 plt.show() 
 
-## model for a hypothetical cpu 
-x = np.array(data[['# of CPU Cores', '# of Threads', 'Base Clock(GHz)', 'L1 Cache (KB)', 'L2 Cache(MB)', 'L3 Cache(MB)']])
-y = np.array(data["1kU Pricing(USD)"])  
+## model for a hypothetical cpu  
+##removed caches
+x = np.array(cleanedData2[['# of CPU Cores', '# of Threads', 'Base Clock(GHz)']])
+y = np.array(cleanedData2["1kU Pricing(USD)"])  
 xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.5, random_state=42)
 
 model = PassiveAggressiveRegressor()
 model.fit(xtrain, ytrain)
 model.score(xtest, ytest)  
-features = np.array([[8, 16, 4, 120, 11.5, 24.5]])
+features = np.array([[8, 16, 4]])
 prediction = model.predict(features)   
-## filters the prediction model such that 0 or less values are not considered as they are invalid
-while prediction <= 0:   
-    prediction = model.predict(features)
 print(prediction)  
 
 
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15, 10), random_state=42, batch_size = 300) 
-clf.fit(data[['# of CPU Cores', '# of Threads', 'Base Clock(GHz)', 'L1 Cache (KB)', 'L2 Cache(MB)', 'L3 Cache(MB)']].values, data["1kU Pricing(USD)"].values)   
-MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(42, 10), random_state=42, batch_size = 300)
+clf = MLPClassifier(solver='lbfgs',max_iter=1600, alpha=1e-5, hidden_layer_sizes=(8, 6, 2), random_state=42, batch_size = 80) 
+clf.fit(cleanedData2[['# of CPU Cores', '# of Threads', 'Base Clock(GHz)']].values, cleanedData2["1kU Pricing(USD)"].values)   
+MLPClassifier(solver='lbfgs',max_iter=1600 ,alpha=1e-5, hidden_layer_sizes=(8,6 , 2), random_state=42, batch_size = 80)
 prediction2 = clf.predict(features)  
-while prediction2 <= 0:   
-    prediction2 = clf.predict(features)
 print(prediction2)
  
